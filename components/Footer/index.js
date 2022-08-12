2f425e2f97efd34a1ccc517a39efa15d2b8ccff2
lvkkhang <lvkkhang@gmail.com>
2022-08-12 09:29:50 +0700

    logo

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,20 +2,25 @@ import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
 export default function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <div>
-                <Image src='/images/logo1.png' alt="Vercel Logo" width={100} height={100} />
-            </div>
-            <div style={{ flexDirection: 'column', marginLeft: '20px' }}>
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <Image
+          src="/images/logo1.png"
+          alt="Vercel Logo"
+          width={150}
+          height={100}
+        />
+      </div>
+      {/* <div style={{ flexDirection: 'column', marginLeft: '20px' }}>
                 <div className="fs-18 fw-500">
-                    <span>Box A17-A19 cemtrum polskie</span>
-                </div>
-                {/* <div style={{ marginleft: 30 }}>
+                    <span>Box A17-A19 cemtrum polskie</span> */}
+      {/* </div> */}
+      {/* <div style={{ marginleft: 30 }}>
                     <div><span className="fw-600">Giờ mở cửa:</span>&nbsp;09:00 AM</div>
                     <div><span className="fw-600">Giờ đóng cửa:</span>&nbsp;10:00 PM</div>
                 </div> */}
-            </div>
-        </footer>
-    )
+      {/* </div> */}
+    </footer>
+  )
 }
--- components/category/index.js
+++ components/category/index.js
@@ -6,93 +6,104 @@ import { ShoppingCartOutlined, SearchOutlined } from '@ant-design/icons'
 import { Actions } from '../../redux/reducers/cart'
 import { useDispatch } from 'react-redux'
 import DetailCategory from '../modal/DetailCategory'
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router'
 import { getLanguage } from '../../utils/laguage'
 
 function Category(props) {
-    const t = getLanguage()
-    const { locale } = useRouter();
-    const dispatch = useDispatch();
-    const {
-        data
-    } = props;
+  const t = getLanguage()
+  const { locale } = useRouter()
+  const dispatch = useDispatch()
+  const { data } = props
 
-    const [isVisible, setVisible] = useState(false)
-    const openDetailCategory = () => {
-        setVisible(true)
+  const [isVisible, setVisible] = useState(false)
+  const openDetailCategory = () => {
+    setVisible(true)
+  }
+
+  const closeModalDetail = () => {
+    setVisible(false)
+  }
+
+  const onAddToCart = () => {
+    const dataCart = {
+      id: data.id,
+      size: data.size[0] || null,
+      color: data.color[0] || null,
+      quantity: 1,
+      itemQuantity: data.quantity || 1,
+      name: data.name,
+      price: data.price || 0
     }
+    dispatch(Actions.addToCart(dataCart))
+  }
 
-    const closeModalDetail = () => {
-        setVisible(false)
+  const onAddToCartDetail = (data) => {
+    const dataCart = {
+      id: data.id,
+      size: data.size || null,
+      color: data.color || null,
+      itemQuantity: data.itemQuantity || 1,
+      quantity: data.quantity || 1,
+      name: data.name,
+      price: data.price || 0
     }
+    dispatch(Actions.addToCart(dataCart))
+    closeModalDetail()
+  }
 
-    const onAddToCart = () => {
-        const dataCart = {
-            id: data.id,
-            size: data.size[0] || null,
-            color: data.color[0] || null,
-            quantity: 1,
-            itemQuantity: data.quantity || 1,
-            name: data.name,
-            price: data.price || 0
-        }
-        dispatch(Actions.addToCart(dataCart))
-    }
+  return (
+    <>
+      <div className="category-item">
+        <DetailCategory
+          isVisible={isVisible}
+          onClose={closeModalDetail}
+          data={data}
+          onAddToCartDetail={onAddToCartDetail}
+          locale={locale}
+        />
+        <div className="flex-align-center">
+          <img
+            src={data.image[0]}
+            alt="Picture of the author"
+            width={'100%'}
+            // style={{ height: 350, maxWidth: 350 }}
+            style={{ aspectRatio: '1/1' }}
+          />
+        </div>
 
-    const onAddToCartDetail = (data) => {
-        const dataCart = {
-            id: data.id,
-            size: data.size || null,
-            color: data.color || null,
-            itemQuantity: data.itemQuantity || 1,
-            quantity: data.quantity || 1,
-            name: data.name,
-            price: data.price || 0
-        }
-        dispatch(Actions.addToCart(dataCart))
-        closeModalDetail()
-    }
-
-    return (
-        <>
-            <div className='category-item'>
-                <DetailCategory
-                    isVisible={isVisible}
-                    onClose={closeModalDetail}
-                    data={data}
-                    onAddToCartDetail={onAddToCartDetail}
-                    locale={locale}
-                />
-                <div className='flex-align-center'>
-                    <img
-                        src={data.image[0]}
-                        alt="Picture of the author"
-                        width={'100%'}
-                        style={{ height: 400, maxWidth: 300 }}
-                    />
-                </div>
-
-                <div className="category-action-hover">
-                    <div className='action-hover'>
-                        <Tooltip title={t.DETAIL.addToCart} onClick={onAddToCart}>
-                            <Button style={{ backgroundColor: 'transparent' }} icon={<ShoppingCartOutlined style={{ color: '#fff' }} />} size={'large'} />
-                        </Tooltip>
-                        <Tooltip title={t.DETAIL.seeDetail} placement='bottom' onClick={openDetailCategory}>
-                            <Button style={{ backgroundColor: 'transparent', top: 10 }} icon={<SearchOutlined style={{ color: '#fff' }} />} size={'large'} />
-                        </Tooltip>
-                    </div>
-                </div>
-            </div>
-            <div className='fw-500 text-align-center mt-10 fs-16 '>
-                <div className='g-color-black'>
-                {`${data.name[locale]} ${data.code && `(${data.code})`}`}
-                </div>
-                <div className='g-color-blue'>
-                    {`${data.price} ${data.quantity && `(x${data.quantity})`}`} ZŁ
-                </div>
-            </div>
-        </>
-    )
+        <div className="category-action-hover">
+          <div className="action-hover">
+            <Tooltip title={t.DETAIL.addToCart} onClick={onAddToCart}>
+              <Button
+                style={{ backgroundColor: 'transparent' }}
+                icon={<ShoppingCartOutlined style={{ color: '#fff' }} />}
+                size={'large'}
+              />
+            </Tooltip>
+            <Tooltip
+              title={t.DETAIL.seeDetail}
+              placement="bottom"
+              onClick={openDetailCategory}
+            >
+              <Button
+                style={{ backgroundColor: 'transparent', top: 10 }}
+                icon={<SearchOutlined style={{ color: '#fff' }} />}
+                size={'large'}
+              />
+            </Tooltip>
+          </div>
+        </div>
+      </div>
+      <div className="fw-500 text-align-center mt-10 fs-16 ">
+        <div className="g-color-black">
+          {`${data.name[locale]} ${data.code && `(${data.code})`}`}
+        </div>
+        <div className="g-color-blue">
+          {`${data.price} ${data.quantity && `(x${data.quantity})`}`} ZŁ
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Category
